bb823d66c53df9071fc84c4dcb9a905805cad41e
mistakesglue <62752993+mistakesglue@users.noreply.github.com>
2021-02-10 12:51:27 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,6 +11,8 @@ dirIn = "untransformedImages/"  # 未处理照片的文件夹
 dirOut = "transformedImages/"  # 处理过的照片的文件夹
 
 picsNameList = os.listdir(dirIn)  # 将所有未处理的照片名封装成列表
+if '.DS_Store' in picsNameList:  # Mac电脑的毛病
+    picsNameList.remove('.DS_Store')
 
 # 设置for循环，对每张照片处理
 for picName in picsNameList:
